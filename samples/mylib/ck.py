3ce78d2b1749d18c70007fd8756a6b7f2d052756
ckozakai <38804436+ckozakai@users.noreply.github.com>
2019-04-02 09:33:59 +0900

    Made function to get trend file list.

--- samples/mylib/ck.py
+++ samples/mylib/ck.py
@@ -22,3 +22,49 @@ def GetFilelist(gpsstart,gpsend):
         sources.remove(y)
 
     return sources
+
+def GetMtrendFilelist(gpsstart,gpsend):
+    sources = []
+
+    for i in range(int(gpsstart[0:5]),int(gpsend[0:5])+1):
+        dir = '/data/trend/minute/' + str(i) + '/*'
+        source = glob.glob(dir)
+        sources.extend(source)
+        
+    sources.sort()
+        
+    removelist = []
+
+    for x in sources:
+        if int(x[32:42])<(int(gpsstart)-3599):
+            removelist.append(x)
+        if int(x[32:42])>int(gpsend):
+            removelist.append(x)
+
+    for y in removelist:
+        sources.remove(y)
+
+    return sources
+
+def GetStrendFilelist(gpsstart,gpsend):
+    sources = []
+
+    for i in range(int(gpsstart[0:5]),int(gpsend[0:5])+1):
+        dir = '/data/trend/second/' + str(i) + '/*'
+        source = glob.glob(dir)
+        sources.extend(source)
+        
+    sources.sort()
+        
+    removelist = []
+
+    for x in sources:
+        if int(x[32:42])<(int(gpsstart)-599):
+            removelist.append(x)
+        if int(x[32:42])>int(gpsend):
+            removelist.append(x)
+
+    for y in removelist:
+        sources.remove(y)
+
+    return sources

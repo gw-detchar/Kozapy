2a39423152bdf37b3d96fac697946873fd493d4e
Alex Urban <alexander.urban@ligo.org>
2019-09-30 15:43:11 +0900

    Add silent run dq setting.

--- samples/test.py
+++ samples/test.py
@@ -1,4 +1,4 @@
-'''Make spectrogram.
+'''Make coherencegram.
 '''
 
 __author__ = "Chihiro Kozakai"
@@ -11,8 +11,6 @@ import subprocess
 import glob
 from gwpy.timeseries import TimeSeries
 from gwpy.timeseries import TimeSeriesDict
-from gwpy.segments import DataQualityFlag
-
 from matplotlib import pylab as pl
 from gwpy.detector import Channel
 from matplotlib import cm
@@ -27,16 +25,15 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Make coherencegram.')
 parser.add_argument('-o','--outdir',help='output directory.',default='/tmp')
-parser.add_argument('-c','--channel',help='channel.',required=True)
+parser.add_argument('-r','--refchannel',help='main reference channel.',required=True)
+parser.add_argument('-c','--channel',help='compared channel.',required=True)
 parser.add_argument('-s','--gpsstart',help='GPS starting time.',required=True)
 parser.add_argument('-e','--gpsend',help='GPS ending time.',required=True
 )
-parser.add_argument('-w','--whitening',help='Apply whitening.',action='store_true')
-
 parser.add_argument('-f','--fftlength',help='FFT length.',type=float,default=1.)
 parser.add_argument('--fmin',help='Frequency minimum limit.',type=float,default=-1.)
 parser.add_argument('--fmax',help='Frequency maximum limit.',type=float,default=8000.)
-parser.add_argument('--stride',help='Stride of the coherencegram.',type=float,default=1.)
+parser.add_argument('--stride',help='Stride of the coherencegram.',type=float,default=10.)
 parser.add_argument('-i','--index',help='It will be added to the output file name.',default='test')
 
 parser.add_argument('-l','--lchannel',help='Make locked segment bar plot.',default='')
@@ -52,23 +49,17 @@ kamioka = args.kamioka
 
 outdir=args.outdir
 
-whitening=args.whitening
-
+refchannel=args.refchannel
 channel=args.channel
-
 if kamioka:
     latexchname = channel.replace('_','\_')
+    latexrefchname = refchannel.replace('_','\_')
 else:
     latexchname = channel
+    latexrefchname = refchannel
 
-if whitening:
-    latexchname += " whitened"
-latexchname += " spectrogram"
 gpsstart=args.gpsstart
 gpsend=args.gpsend
-
-dpi=args.dpi
-
 index=args.index
 stride=args.stride
 fft=args.fftlength
@@ -77,84 +68,90 @@ ol=fft/2.  #  overlap in FFTs.
 fmin=args.fmin
 fmax=args.fmax
 
-lchannel=args.lchannel
-lnumber=args.lnumber
-llabel=args.llabel
+lchannel = args.lchannel
+lnumber = args.lnumber
+llabel = args.llabel
 
-lflag=bool(lchannel)
+dpi=args.dpi
 
 
 
-if fft > stride:
-    print('Warning: stride is shorter than fft length. Set stride=fft')
-    stride=fft
+# If lflag is True, locked segments is plotted.            
+lflag = bool(lchannel)
+
+if fft > stride/2.:
+    print('Warning: stride is shorter than fft length. Set stride=fft*2.')
+    stride=fft*2.
     
-#unit = r'Amplitude [$\sqrt{\mathrm{Hz}^{-1}}$]'
-unit = r'Amplitude [1/rHz]'
-if channel.find('ACC') != -1:
-    unit = 'Acceleration [m/s^2/rHz]'
-elif channel.find('MIC') != -1:
-    unit = 'Sound [Pa/rHz]'
-
 # Get data from frame files
-if kamioka:
-    data = TimeSeries.fetch(channel,gpsstart,gpsend,host='k1nds0',port=8088)
+if kamioka:    
+    sources = mylib.GetFilelist_Kamioka(gpsstart,gpsend)
 else:
     sources = mylib.GetFilelist(gpsstart,gpsend)
-    data = TimeSeries.read(sources,channel,format='gwf.lalframe',start=float(gpsstart),end=float(gpsend))
+    
+channels = [refchannel, channel]
 
-if fft <= data.dt.value:
-    fft=2*data.dt.value
-    ol=fft/2.  #  overlap in FFTs. 
+data = TimeSeriesDict.read(sources,channels,format='gwf.lalframe',start=float(gpsstart),end=float(gpsend))
+
+
+ref = data[refchannel]
+com = data[channel]
+
+if fft < ref.dt.value:
+    fft=2*ref.dt.value
+    ol=fft/2.  #  overlap in FFTs.                        
+    stride=2*fft
+    print("Given fft/stride was bad against the sampling rate. Automatically set to:")
+    print("fft="+str(fft))
+    print("ol="+str(ol))
+    print("stride="+str(stride))
+if fft < com.dt.value:
+    fft=2*com.dt.value
+    ol=fft/2.  #  overlap in FFTs.                        
     stride=2*fft
     print("Given fft/stride was bad against the sampling rate. Automatically set to:")
     print("fft="+str(fft))
     print("ol="+str(ol))
     print("stride="+str(stride))
 
-if whitening:
-    white = data.whiten(fftlength=fft,overlap=ol,fduration=stride)
-    whitespectrogram = white.spectrogram(stride,fftlength=fft,overlap=ol) ** (1/2.)
+#coh = ref.coherence_spectrogram(com,stride,fftlength=fft,overlap=ol)
+coh = ref.csd(com,fftlength=fft,overlap=ol)
+print(coh)
 
-    sgplot=whitespectrogram.plot(figsize = (12, 8))
-else:
-    spectrogram = data.spectrogram(stride,fftlength=fft,overlap=ol) ** (1/2.)
-    sgplot=spectrogram.plot(figsize = (12, 8),norm='log')
 
-ax = sgplot.gca()
+
+# Get absolute
+coh.abs()
+print(coh)
+# The time axis of coherencegram seems buggy in this version. Temporal fix is needed.
+#coh.dx = stride
+
+#cohplot=coh.plot(figsize = (12, 8),vmin=0.,vmax=1.)
+cohplot=coh.plot(figsize = (12, 8))
+ax = cohplot.gca()
 ax.set_ylabel('Frequency [Hz]')
 ax.set_yscale('log')
-ax.set_title(latexchname)
-if fmin < 0:    
-    fmin = 0.8/fft
+ax.set_title(latexrefchname + ' ' + latexchname)
 
-print(type(fmin))
-print(type(fmax))
-print(fmin)
-print(fmax)
-    
+if fmin < 0:
+    fmin = 0.8/fft
 ax.set_ylim(fmin,fmax)
 
-if whitening:
-    sgplot.add_colorbar(cmap='YlGnBu_r',label='Arbitrary')
-    fname = outdir + '/' + channel + '_whiteningspectrogram_'+ gpsstart + '_' + gpsend +'_' + index +'.png'
-else:
-    sgplot.add_colorbar(cmap='YlGnBu_r',label=unit,log=True)
-    fname = outdir + '/' + channel + '_spectrogram_'+ gpsstart + '_' + gpsend +'_' + index +'.png'
+#cohplot.add_colorbar(cmap='YlGnBu_r',label='Coherence')
 
 if lflag:
     ldata = TimeSeries.read(sources,lchannel,format='gwf.lalframe',start=float(gpsstart),end=float(gpsend))
     locked = ldata == lnumber
     flag = locked.to_dqflag(name = '', label = llabel, round = True)
-    sgplot.add_state_segments(flag)
+    cohplot.add_state_segments(flag)
 else:
     pass
 
+fname = outdir + '/' + refchannel + '_' + channel + '_coherence_'+ gpsstart + '_' + gpsend +'_' + index +'.png'
+cohplot.savefig(fname,dpi=dpi)
 
-sgplot.savefig(fname,dpi=dpi)
-
-sgplot.clf()
-sgplot.close()
+cohplot.clf()
+cohplot.close()
 
 print(fname)
 print('Successfully finished !')

e3fb6b503119804514f39dacbc6cf37eca25c66f
ckozakai <38804436+ckozakai@users.noreply.github.com>
2019-11-07 17:06:34 +0900

    Qtransform is improved. Q can be specified. Z axis is log scale.

--- samples/batch_qtransform.py
+++ samples/batch_qtransform.py
@@ -32,6 +32,7 @@ parser.add_argument('-s','--gpsstart',help='GPS starting time.',required=True)
 parser.add_argument('-e','--gpsend',help='GPS ending time.',required=True)
 parser.add_argument('-t','--time',help='Plot time duration.',type=float,default=None)
 parser.add_argument('-f','--fmin',help='frequency range.',default=8 )
+parser.add_argument('-q','--q',help='Q range.',default=-1 )
 
 parser.add_argument('-i','--index',help='It will be added to the output file name.',default='test')
 
@@ -40,6 +41,7 @@ parser.add_argument('--llabel',help='Label of the locked segment bar plot.',defa
 parser.add_argument('-n','--lnumber',help='The requirement for judging locked. lchannel==lnumber will be used as locked.',default=99,type=int)
 parser.add_argument('-k','--kamioka',help='Flag to run on Kamioka server.',action='store_true')
 parser.add_argument('--dpi',help='Plot resolution. dot per inch.',type=int,default=100)
+
 # define variables
 args = parser.parse_args()
 outdir=args.outdir
@@ -57,17 +59,17 @@ dpi=args.dpi
 #margin=40
 margin=4
 
-# To see ~8Hz < f
-#if float(gpsend)-float(gpsstart)<40:
-#    margin=20
-
 gpsstartmargin=float(gpsstart)-margin
 gpsendmargin=float(gpsend)+margin
 
+qmin = 4
+qmax = 100
+
+if args.q > 0:
+    qmin = args.q
+    qmax = args.q
 
 index=args.index
-#fft=args.fftlength
-#ol=fft/2.  #  overlap in FFTs. 
 
 lchannel=args.lchannel
 lnumber=args.lnumber
@@ -91,31 +93,27 @@ data = TimeSeries.read(sources,channel,format='gwf.lalframe',start=float(gpsstar
 #if maxf > 1./data.dt.value/4.:
 fmax=1./data.dt.value/4.
 
-qgram = data.q_transform(outseg=[float(gpsstart),float(gpsend)],frange=(fmin,fmax),qrange=(4,100),gps=float(gpsstart)/2.+float(gpsend)/2.,logf=True)
+#qgram = data.q_transform(outseg=[float(gpsstart),float(gpsend)],frange=(fmin,fmax),qrange=(4,100),gps=float(gpsstart)/2.+float(gpsend)/2.,logf=True)
+qgram = data.q_transform(outseg=[float(gpsstart),float(gpsend)],frange=(fmin,fmax),qrange=(qmin,qmax),gps=float(gpsstart)/2.+float(gpsend)/2.,logf=True)
 
 # default parameter
 #qrange=(4, 64), frange=(0, inf), gps=None, search=0.5, tres='<default>', fres='<default>', logf=False, norm='median', mismatch=0.2, outseg=None, whiten=True, fduration=2, highpass=None, **asd_kw 
 #plot=qgram.plot(figsize = (12, 8),vmin=0.,vmax=25.)
-plot=qgram.plot(figsize = (12, 8),vmin=0.)
+plot=qgram.plot(figsize = (12, 8))
 
 ax = plot.gca()
 ax.set_ylabel('Frequency [Hz]')
 ax.set_yscale('log')
 ax.set_title(latexchname+" Q-transform")
 
-plot.add_colorbar(cmap='YlGnBu_r',label="Normalized energy")
+plot.add_colorbar(cmap='YlGnBu_r',label="Normalized energy",log=True, clim=[0.1,None])
 fname = outdir + '/' + channel + '_qtransform_'+ gpsstart + '_' + gpsend +'_' + index +'.png'
 
 if lflag:
-#    ldata = TimeSeries.read(sources,lchannel,format='gwf.lalframe',start=float(gpsstart),end=float(gpsend))
-#    locked = ldata == lnumber
-#    flag = locked.to_dqflag(name = '', label = llabel, round = True)
-
     flag = mylib.GetDQFlag(float(gpsstart),float(gpsend),config=llabel, kamioka=kamioka)
     plot.add_state_segments(flag)
 else:
     pass
-
 
 plot.savefig(fname,dpi=dpi)
 
